fb31d48ec5c6b794b09fe56ff4add349ae455d61
andikas <andpuji27@gmail.com>
2023-02-02 20:26:18 +0700

    migrate article item to compose

--- app/src/main/java/com/andikas/burogu/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/andikas/burogu/adapter/ArticleAdapter.kt
@@ -2,12 +2,35 @@ package com.andikas.burogu.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bookmark
+import androidx.compose.material.icons.rounded.BookmarkBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.recyclerview.widget.RecyclerView
+import coil.compose.AsyncImage
 import com.andikas.burogu.R
 import com.andikas.burogu.data.model.Article
 import com.andikas.burogu.databinding.ItemArticleBinding
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.andikas.burogu.utils.Extensions.Poppins
 
 class ArticleAdapter(
     private val articles: List<Article>,
@@ -16,40 +39,164 @@ class ArticleAdapter(
 ) : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return ViewHolder(binding, onClick, onBookmarkClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val article = articles[position]
-
-        if (article.isDummy) {
-            Glide.with(holder.itemView)
-                .load(article.imagePath)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(R.drawable.ic_no_image)
-                .centerCrop()
-                .into(holder.binding.imgArticle)
-        } else {
-            Glide.with(holder.itemView)
-                .load(article.imagePath)
-                .error(R.drawable.ic_no_image)
-                .centerCrop()
-                .into(holder.binding.imgArticle)
-        }
-
-        if (article.isBookmarked) {
-            holder.binding.btnBookmark.setImageResource(R.drawable.ic_bookmark)
-        } else {
-            holder.binding.btnBookmark.setImageResource(R.drawable.ic_unbookmark)
-        }
-
-        holder.binding.txtTitle.text = article.title
-        holder.binding.txtAuthor.text = article.author
-        holder.binding.btnBookmark.setOnClickListener { onBookmarkClick(article) }
-        holder.itemView.setOnClickListener { onClick(article) }
+        holder.bind(article)
     }
 
     override fun getItemCount(): Int = articles.size
 
-    inner class ViewHolder(var binding: ItemArticleBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(
+        private var binding: ItemArticleBinding,
+        private val onClick: (Article) -> Unit,
+        private val onBookmarkClick: (Article) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            binding.composeView.setContent {
+                MaterialTheme {
+                    ArticleItem(
+                        article.id,
+                        article.imagePath,
+                        article.title,
+                        article.content,
+                        article.createdDate,
+                        article.author,
+                        article.isBookmarked,
+                        article.isDummy,
+                        onClick,
+                        onBookmarkClick,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticleItem(
+    id: Long?,
+    imagePath: String?,
+    title: String?,
+    content: String?,
+    createdDate: String?,
+    author: String?,
+    isBookmarked: Boolean,
+    isDummy: Boolean,
+    onClick: (Article) -> Unit,
+    onBookmarkClick: (Article) -> Unit
+) {
+    val article = Article(id, imagePath, title, content, createdDate, author, isBookmarked, isDummy)
+
+    ConstraintLayout(
+        modifier = Modifier
+            .clickable {
+                onClick(article)
+            }
+    ) {
+        val (articleImage, stackImage, titleText, authorText, bookmarkButton) = createRefs()
+
+        AsyncImage(
+            model = imagePath,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .constrainAs(articleImage) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        AsyncImage(
+            model = R.drawable.bg_stack,
+            contentDescription = null,
+            modifier = Modifier
+                .constrainAs(stackImage) {
+                    start.linkTo(articleImage.start)
+                    top.linkTo(articleImage.top)
+                    end.linkTo(articleImage.end)
+                    bottom.linkTo(articleImage.bottom)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+        )
+
+        Text(
+            text = title ?: "",
+            fontSize = 18.sp,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Start,
+            maxLines = 2,
+            fontFamily = Poppins,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.primaryLight),
+            modifier = Modifier
+                .constrainAs(titleText) {
+                    start.linkTo(articleImage.start, margin = 16.dp)
+                    end.linkTo(bookmarkButton.start, margin = 30.dp)
+                    bottom.linkTo(authorText.top)
+                    width = Dimension.fillToConstraints
+                }
+        )
+
+        Text(
+            text = author ?: "",
+            fontSize = 14.sp,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            fontFamily = Poppins,
+            fontWeight = FontWeight.Medium,
+            color = colorResource(id = R.color.primaryLight),
+            modifier = Modifier
+                .constrainAs(authorText) {
+                    start.linkTo(titleText.start)
+                    end.linkTo(titleText.end)
+                    bottom.linkTo(bookmarkButton.bottom)
+                    width = Dimension.fillToConstraints
+                }
+        )
+
+        IconButton(
+            onClick = {
+                onBookmarkClick(article)
+            },
+            modifier = Modifier
+                .size(24.dp)
+                .constrainAs(bookmarkButton) {
+                    end.linkTo(parent.end, margin = 16.dp)
+                    bottom.linkTo(parent.bottom, margin = 16.dp)
+                }
+        ) {
+            Icon(
+                imageVector = if (isBookmarked) Icons.Rounded.Bookmark else Icons.Rounded.BookmarkBorder,
+                tint = colorResource(id = R.color.primaryLight),
+                contentDescription = "Tambahkan ke bookmark"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArticleItemPreview() {
+    MaterialTheme {
+        ArticleItem(
+            id = 1,
+            imagePath = "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:bb2a73c0f97d1d5c4292af18d2334e5020221025171536.jpeg",
+            title = "lorem ipsum dolor sit amet",
+            content = "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:bb2a73c0f97d1d5c4292af18d2334e5020221025171536.jpeghttps://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:bb2a73c0f97d1d5c4292af18d2334e5020221025171536.jpeghttps://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:bb2a73c0f97d1d5c4292af18d2334e5020221025171536.jpeg",
+            createdDate = "Daw",
+            author = "dawdawdaw",
+            isBookmarked = false,
+            isDummy = false,
+            onClick = { },
+            onBookmarkClick = { }
+        )
+    }
 }
