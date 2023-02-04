c6baaa74fa987f6b06f50ccbabbda5e0d4cdba0c
andikas <andpuji27@gmail.com>
2023-02-04 21:13:33 +0700

    migrate detail screen to compose

--- app/src/main/java/com/andikas/burogu/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/andikas/burogu/ui/detail/DetailActivity.kt
@@ -1,26 +1,47 @@
 package com.andikas.burogu.ui.detail
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
+import coil.compose.AsyncImage
 import com.andikas.burogu.R
 import com.andikas.burogu.data.local.ArticleDatabase
 import com.andikas.burogu.data.model.Article
 import com.andikas.burogu.databinding.ActivityDetailBinding
 import com.andikas.burogu.ui.edit.EditActivity
 import com.andikas.burogu.ui.home.HomeActivity
+import com.andikas.burogu.utils.Extensions.Poppins
+import com.andikas.burogu.utils.Extensions.Roboto
 import com.andikas.burogu.utils.Extensions.hideActionBar
 import com.andikas.burogu.utils.Extensions.navigateTo
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+@Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity(), DetailView {
     private lateinit var binding: ActivityDetailBinding
     private lateinit var presenter: DetailPresenterImp
@@ -30,42 +51,22 @@ class DetailActivity : AppCompatActivity(), DetailView {
         super.onCreate(savedInstanceState)
 
         binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         hideActionBar()
 
         database = ArticleDatabase.getInstance(this)
         presenter = DetailPresenterImp(database!!, this, Dispatchers.Main)
-        val extraArticle = intent.getParcelableExtra<Article>("extraArticle")!!
-
+        val extraArticle = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("extraArticle", Article::class.java)!!
+        } else {
+            intent.getParcelableExtra("extraArticle")!!
+        }
         presenter.loadArticleDetails(extraArticle)
-        binding.btnBack.setOnClickListener { onBackPressed() }
-        binding.btnBookmark.setOnClickListener {
-            presenter.setArticleBookmark(
-                extraArticle,
-                !extraArticle.isBookmarked
-            )
-        }
-        binding.btnEdit.setOnClickListener {
-            navigateTo(
-                EditActivity::class.java,
-                "extraArticle",
-                extraArticle
-            )
-        }
-        binding.btnDelete.setOnClickListener { presenter.showAlert(extraArticle) }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun showDeleteAlert(article: Article) {
-        val textDialogTitle = TextView(this)
-        textDialogTitle.text = "Hapus"
-        textDialogTitle.textSize = 18f
-        textDialogTitle.isAllCaps = true
-        textDialogTitle.typeface = ResourcesCompat.getFont(this, R.font.poppins_semi_bold)
-        textDialogTitle.setTextColor(ContextCompat.getColor(this, R.color.primaryRed))
-        textDialogTitle.setPadding(45, 50, 45, 50)
-
         val builder = AlertDialog.Builder(this, R.style.CustomDialog)
-            .setCustomTitle(textDialogTitle)
+            .setTitle("Hapus")
             .setMessage("Anda yakin ingin menghapus artikel?")
             .setCancelable(true)
             .setPositiveButton("Hapus") { _, _ ->
@@ -84,30 +85,194 @@ class DetailActivity : AppCompatActivity(), DetailView {
     }
 
     override fun loadDetails(article: Article) {
-        if (article.isBookmarked) {
-            binding.btnBookmark.setImageResource(R.drawable.ic_bookmark)
-        } else {
-            binding.btnBookmark.setImageResource(R.drawable.ic_unbookmark)
+        setContent {
+            MaterialTheme {
+                ArticleDetailContent(
+                    imagePath = article.imagePath,
+                    title = article.title,
+                    content = article.content,
+                    createdDate = article.createdDate,
+                    author = article.author,
+                    isBookmarked = article.isBookmarked,
+                    onBack = {
+                        onBackPressedDispatcher.onBackPressed()
+                    },
+                    onBookmarkClick = {
+                        presenter.setArticleBookmark(
+                            article,
+                            !article.isBookmarked
+                        )
+                    },
+                    onEditClick = {
+                        navigateTo(
+                            EditActivity::class.java,
+                            "extraArticle",
+                            article
+                        )
+                    },
+                    onDeleteClick = {
+                        presenter.showAlert(article)
+                    }
+                )
+            }
         }
+    }
+}
 
-        if (article.isDummy) {
-            Glide.with(this)
-                .load(article.imagePath)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(R.drawable.ic_no_image)
-                .centerCrop()
-                .into(binding.imgArticle)
-        } else {
-            Glide.with(this)
-                .load(article.imagePath)
-                .error(R.drawable.ic_no_image)
-                .centerCrop()
-                .into(binding.imgArticle)
+@Composable
+fun ArticleDetailContent(
+    imagePath: String?,
+    title: String?,
+    content: String?,
+    createdDate: String?,
+    author: String?,
+    isBookmarked: Boolean,
+    onBack: () -> Unit,
+    onBookmarkClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(Color.White)
+            .padding(20.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 160.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Button(
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primaryDark)),
+                    onClick = { onBack() },
+                    modifier = Modifier,
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.ArrowBackIos,
+                        tint = Color.White,
+                        contentDescription = "Kembali",
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        onBookmarkClick()
+                    },
+                    modifier = Modifier
+                        .size(24.dp)
+                ) {
+                    androidx.compose.material.Icon(
+                        imageVector = if (isBookmarked) Icons.Rounded.Bookmark else Icons.Rounded.BookmarkBorder,
+                        tint = colorResource(id = R.color.primaryDark),
+                        contentDescription = "Tambahkan ke bookmark"
+                    )
+                }
+            }
+            Text(
+                text = title ?: "",
+                fontSize = 24.sp,
+                fontFamily = Poppins,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                color = colorResource(id = R.color.primaryDark),
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = author ?: "",
+                fontSize = 14.sp,
+                fontFamily = Roboto,
+                textAlign = TextAlign.Start,
+                color = colorResource(id = R.color.neutralLight),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+            )
+            Text(
+                text = createdDate ?: "",
+                fontSize = 14.sp,
+                fontFamily = Roboto,
+                textAlign = TextAlign.Start,
+                color = colorResource(id = R.color.neutralLight),
+                modifier = Modifier
+            )
+            AsyncImage(
+                model = imagePath,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(230.dp)
+                    .padding(top = 30.dp)
+            )
+            Text(
+                text = content ?: "",
+                fontSize = 16.sp,
+                fontFamily = Roboto,
+                textAlign = TextAlign.Start,
+                color = colorResource(id = R.color.neutralDark),
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth()
+            )
         }
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 32.dp),
+        ) {
+            FloatingActionButton(
+                containerColor = colorResource(id = R.color.primaryDark),
+                contentColor = Color.White,
+                onClick = { onEditClick() },
+                modifier = Modifier,
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Edit,
+                    contentDescription = "Sunting"
+                )
+            }
+            FloatingActionButton(
+                containerColor = colorResource(id = R.color.primaryDark),
+                contentColor = Color.White,
+                onClick = { onDeleteClick() },
+                modifier = Modifier
+                    .padding(top = 12.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Delete,
+                    contentDescription = "Hapus"
+                )
+            }
+        }
+    }
+}
 
-        binding.txtTitle.text = article.title
-        binding.txtCreatedDate.text = article.createdDate
-        binding.txtAuthor.text = article.author
-        binding.txtContent.text = article.content
+@Preview(
+    device = Devices.PIXEL_4_XL,
+    showSystemUi = false,
+)
+@Composable
+fun ArticleDetailContentPreview() {
+    MaterialTheme {
+        ArticleDetailContent(
+            "",
+            "title",
+            "content",
+            "created date",
+            "author",
+            true,
+            {},
+            {},
+            {},
+            {}
+        )
     }
 }
