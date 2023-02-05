effab0914e826fa81fdeef13338e7e85a3bb8319
andikas <andpuji27@gmail.com>
2023-02-05 09:39:49 +0700

    make article item rounded

--- app/src/main/java/com/andikas/burogu/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/andikas/burogu/adapter/ArticleAdapter.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.rounded.Bookmark
 import androidx.compose.material.icons.rounded.BookmarkBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -86,12 +88,14 @@ fun ArticleItem(
     isBookmarked: Boolean,
     isDummy: Boolean,
     onClick: (Article) -> Unit,
-    onBookmarkClick: (Article) -> Unit
+    onBookmarkClick: (Article) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     val article = Article(id, imagePath, title, content, createdDate, author, isBookmarked, isDummy)
 
     ConstraintLayout(
-        modifier = Modifier
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
             .clickable {
                 onClick(article)
             }
